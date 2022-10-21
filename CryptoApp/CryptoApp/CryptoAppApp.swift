ee23103dc1188fb93a5bf125d250e15ca3063d4c
Umut Erturk <umutc.erturk@gmail.com>
2022-10-21 21:05:11 +0300

    Initial Commit

---
+++ CryptoApp/CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Umut Can Ertürk on 21.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
